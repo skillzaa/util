9c2cc0aa65b75c328e02aa36a0e251f7cfb92c6c
Bilal Tariq <skillzaa.com@gmail.com>
2021-06-02 00:02:03 +0500

    3

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 mod attributesenum;
 mod animateresponses;
 mod counter;
+use std::fmt;
 // use std::fmt::{Debug,Formatter,Result};
 pub use attributesenum::AttributesEnum;
 pub use animateresponses::AnimateResponses;
 pub use counter::BaseCounter;
-pub trait Animatable {
+pub trait Animatable: fmt::Display {
     fn animate(&self,time_ms:u128)->Option<AnimateResponses>;
 }
 
