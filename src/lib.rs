b220b7e5fb971db97b7595dc43907602b856ae22
Bilal Tariq <skillzaa.com@gmail.com>
2021-06-01 23:26:39 +0500

    0.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
 mod attributesenum;
 mod animateresponses;
 mod counter;
-
+// use std::fmt::{Debug,Formatter,Result};
 pub use attributesenum::AttributesEnum;
 pub use animateresponses::AnimateResponses;
 pub use counter::BaseCounter;
 pub trait Animatable {
     fn animate(&self,time_ms:u128)->Option<AnimateResponses>;
 }
+
+// impl Debug for Animatable {
+//     fn fmt(&sel, f: &mut Formatter<'_>) -> Result {
+       
+        
+//     }
+// }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-use bilzaa2dutil::{BaseCounter,AttributesEnum,Animatable};
-fn main(){
+// use bilzaa2dutil::{BaseCounter,AttributesEnum,Animatable};
+// fn main(){
 
-let a = BaseCounter::new(0,
-    10,1,100,AttributesEnum::Width);
-match a {
-    Some(aa)=>{
-        let jj = aa.animate(20);
-        match jj {
-            Some(jjj)=>println!("{:?}",jjj),
-            None=>(),
-        }
+// let a = BaseCounter::new(0,
+//     10,1,100,AttributesEnum::Width);
+// match a {
+//     Some(aa)=>{
+//         let jj = aa.animate(20);
+//         match jj {
+//             Some(jjj)=>println!("{:?}",jjj),
+//             None=>(),
+//         }
         
-    },
-    None=>(),
-}
+//     },
+//     None=>(),
+// }
 
 
 
 
-}//main
+// }//main
